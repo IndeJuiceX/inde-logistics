6f86aa7cb92e76b8665a1acff0dc3f7037021eae
amannan-indejuice <abdul@indejuice.com>
2024-09-17 16:00:44 +0100

    Refactor error handling in Home component for failed product upload

--- app/page.js
+++ app/page.js
@@ -105,7 +105,7 @@ export default function Home() {
           message: result.error
         });
       } else {
-        alert(`Failed to upload products for vendor ${vendorId}`);
+        alert(`Failed to upload products for vendor ${vendorId} ${result.error}`);
       }
     } catch (error) {
       alert(`Error uploading products for vendor ${vendorId}`);
