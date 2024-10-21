6448cdefa4e56895369ae3420182da3f1f58f42e
amannan-indejuice <abdul@indejuice.com>
2024-10-21 13:48:48 +0100

    Refactor code: Remove console.log statement in authenticateAndAuthorize function

--- services/utils/index.js
+++ services/utils/index.js
@@ -27,7 +27,6 @@ export async function authenticateAndAuthorize(request) {
     try {
       // Verify the token and extract user info
       user = decodeToken(token);
-      console.log(user)
     } catch (error) {
       console.error('Error verifying token:', error);
       return { authorized: false, status: 401 }; // Unauthorized
