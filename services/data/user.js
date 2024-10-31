43a43da4eb50c37091c44fbf2629559bced97f9d
amannan-indejuice <abdul@indejuice.com>
2024-10-31 14:41:01 +0000

    Refactor: Update user-related functions and variables
    - Update import statement for getAllUsers in api/v1/admin/users/route.js
    - Add 'birthdate' field to user registration in api/auth/register/route.js
    - Update saveUser function in services/data/user.js to include 'created_at' field
    - Remove unused getUserByEmail function in services/data/user.js
    - Update getAllVendorUsers function in services/data/user.js to getAllUsers and update query parameters

--- services/data/user.js
+++ services/data/user.js
@@ -2,46 +2,57 @@ import { getItem, queryItems, putItem, deleteItem } from '../dynamo/wrapper';
 
 // Function to retrieve a single vendor by ID
 export const getUserByEmail = async (email) => {
-    return await getItem(`USER#${email}`, `USER#${email}`);
+  return await getItem(`USER#${email}`, `USER#${email}`);
 };
 
 // Function to add or update a vendor
 export const saveUser = async (userData) => {
-    const userItem = {
-        entity_type: 'User',
-        ...userData,
-    };
+  const userItem = {
+    entity_type: 'User',
+    ...userData,
+  };
 
-    return await putItem(userItem);
+  return await putItem(userItem);
 };
 
-export const getAllVendorUsers = async() => {
-    const params = {
-        IndexName: 'gs1_vendor_index',
-        KeyConditionExpression: 'entity_type = :entityType',
-        ExpressionAttributeValues: {
-            ':entityType': 'User',
-        },
-    };
-    return await queryItems(params);
+export const getAllVendorUsers = async () => {
+  const params = {
+    IndexName: 'gs1_vendor_index',
+    KeyConditionExpression: 'entity_type = :entityType',
+    ExpressionAttributeValues: {
+      ':entityType': 'User',
+    },
+  };
+  return await queryItems(params);
+}
+
+export const getAllUsers = async () => {
+  const params = {
+    IndexName: 'user_entity_type_index',
+    KeyConditionExpression: 'entity_type = :entityType',
+    ExpressionAttributeValues: {
+      ':entityType': 'User',
+    },
+  };
+  return await queryItems(params);
 }
 
 export const getVendorUsers = async (vendorId) => {
-    // Base query parameters
-    const params = {
-      IndexName: 'gs1_vendor_index',
-      KeyConditionExpression: 'entity_type = :entityType',
-      ExpressionAttributeValues: {
-        ':entityType': 'User',
-      },
-    };
-  
-    // If vendorId is provided, add it to the KeyConditionExpression and ExpressionAttributeValues
-    if (vendorId) {
-      params.KeyConditionExpression += ' AND vendor_id = :vendorId';
-      params.ExpressionAttributeValues[':vendorId'] = vendorId;
-    }
-  
-    // Query the DynamoDB index
-    return await queryItems(params);
+  // Base query parameters
+  const params = {
+    IndexName: 'gs1_vendor_index',
+    KeyConditionExpression: 'entity_type = :entityType',
+    ExpressionAttributeValues: {
+      ':entityType': 'User',
+    },
   };
+
+  // If vendorId is provided, add it to the KeyConditionExpression and ExpressionAttributeValues
+  if (vendorId) {
+    params.KeyConditionExpression += ' AND vendor_id = :vendorId';
+    params.ExpressionAttributeValues[':vendorId'] = vendorId;
+  }
+
+  // Query the DynamoDB index
+  return await queryItems(params);
+};
