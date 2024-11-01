c45d98f5fd16debeea1c1fc8885a7030d8f47fe2
amannan-indejuice <abdul@indejuice.com>
2024-11-01 16:21:03 +0000

    Refactor: Remove unused Helper and SchemaValidation files

--- services/products/Helper.js
+++
@@ -1,8 +0,0 @@
-import crypto from 'crypto';
-
-// Function to generate the SK based on VendorId and vendor_sku
-export const generateSK = (vendorId, vendor_sku) => {
-    const input = `${vendorId}${vendor_sku}`;
-    const hash = crypto.createHash('sha256').update(input).digest('hex');
-    return `PRODUCT#${hash.substring(0, 8)}`; // Take the first 8 characters of the hash
-};
