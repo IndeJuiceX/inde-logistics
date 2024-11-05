bc160348519cb68e24914f484517e2c1f5b22cad
Yukenthan-indejuice <133008282+Yukenthan-indejuice@users.noreply.github.com>
2024-11-05 12:51:04 +0000

    add commnet

--- components/warehouse/unshelved/UnShelvedItems.jsx
+++ components/warehouse/unshelved/UnShelvedItems.jsx
@@ -90,7 +90,7 @@ export default function UnShelvedItems({ vendor, shipmentDetails }) {
                                     </td>
                                     <td className="p-4 text-center text-base text-gray-700">x{item.received ? item.received : '-'}</td>
                                     <td className="p-4 text-center">
-
+                                        {/* this butts are need for the actions */}
                                         {/* <div className="flex items-center justify-center w-20 h-10 border-4 border-green-500 rounded">
                                             <svg className="w-6 h-6 text-green-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                 <path stroke="currentColor" stroke-width="1.5" fill-rule="evenodd" d="M16.707 5.293a1 1 0 00-1.414 0L8 12.586l-3.293-3.293a1 1 0 00-1.414 1.414l4 4a1 1 0 001.414 0l8-8a1 1 0 000-1.414z" clip-rule="evenodd"></path>
