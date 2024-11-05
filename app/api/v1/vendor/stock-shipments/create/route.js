b631b6e8889442692f60253f392073e0c3ce6024
Yukenthan-indejuice <133008282+Yukenthan-indejuice@users.noreply.github.com>
2024-11-05 13:09:49 +0000

    fixed the bugs

--- app/api/v1/vendor/stock-shipments/create/route.js
+++ app/api/v1/vendor/stock-shipments/create/route.js
@@ -9,7 +9,7 @@ const MAX_SIZE_MB = 2 * 1024 * 1024;  // 2MB in bytes
 
 export const POST = withAuthAndLogging(async (request, { params, user }) => {
     try {
-
+        const { searchParams } = new URL(request.url);
         let vendorId = getVendorIdFromRequest(user,searchParams)//user.role === 'admin' ? searchParams.get('vendor_id') : user?.vendor;
 
         // Parse request body
