2b3b5182bfb025b86c1ede5d0ae58e8ff3f7736d
Yukenthan-indejuice <133008282+Yukenthan-indejuice@users.noreply.github.com>
2024-11-11 11:46:56 +0000

    add the received_at

--- components/warehouse/shipments/Shipments.jsx
+++ components/warehouse/shipments/Shipments.jsx
@@ -81,8 +81,8 @@ export default function Shipments({ selectedTap='shipments',vendors, stockShipme
                                         </td>
                                         <td className="py-3 px-6 text-left">{formatDate(shipment.created_at)}</td>
                                         <td className="py-3 px-6 text-center">{shipment.items ? shipment.items.length : 0}</td>
-                                        {/* <td className="py-3 px-6 text-center">537</td> */}
-                                        <td className="py-3 px-6 text-center">-</td>
+                                        {/* received_at  */}
+                                        <td className="py-3 px-6 text-center">{shipment.received_at ? formatDate(shipment.received_at) : '-'}</td>
                                         <td className="py-3 px-6 text-center">-</td>
                                         <td className="py-3 px-6 text-center">-</td>
                                         <td className="py-3 px-6 text-center">
