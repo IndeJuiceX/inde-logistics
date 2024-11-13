6da8b2170cbbb02e24b8ab1705d370be0486e3f5
amannan-indejuice <abdul@indejuice.com>
2024-11-13 15:18:34 +0000

    Refactor: Remove unnecessary logging of transaction items in createShipmentAndUpdateOrder

--- services/data/order-shipment.js
+++ services/data/order-shipment.js
@@ -53,7 +53,6 @@ export const createShipmentAndUpdateOrder = async ( vendorId, orderId ) => {
 
   // Combine the operations into an array
   const transactionItems = [putVendorOrderShipment, updateOrder];
-  console.log('transactionItems', JSON.stringify(transactionItems, null, 2));
   
   // Execute the transaction
   const result = await transactWriteItems(transactionItems);
