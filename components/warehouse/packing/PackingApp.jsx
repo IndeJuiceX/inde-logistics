78e129938f8594899d4370359f0d73eacbc0e2ad
Yukenthan-indejuice <133008282+Yukenthan-indejuice@users.noreply.github.com>
2024-11-18 17:16:26 +0000

    Feature: Integrate ParcelOptions component into PackingApp for improved parcel selection

--- components/warehouse/packing/PackingApp.jsx
+++ components/warehouse/packing/PackingApp.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from '@/styles/warehouse/packing/PackingApp.module.scss';
 import PackingHeader from "@/components/warehouse/packing/PackingHeader";
 import PackingItems from "@/components/warehouse/packing/PackingItems";
+import ParcelOptions from "@/components/warehouse/packing/ParcelOptions";
 
 export default function PackingApp() {
     return (
@@ -16,32 +17,7 @@ export default function PackingApp() {
                 <PackingItems />
 
                 {/* Parcel Options */}
-                <div className={styles.parcelOptions}>
-                    <div className={`${styles.parcel} ${styles.small}`}>
-                        {/* eslint-disable-next-line */}
-                        <img
-                            src="https://dev.indejuice.com/img/wh/parcel_small.png?v=2"
-                            alt="Small Parcel"
-                        />
-                        SMALL PARCEL
-                    </div>
-                    <div className={`${styles.parcel} ${styles.large}`}>
-                        {/* eslint-disable-next-line */}
-                        <img
-                            src="https://dev.indejuice.com/img/wh/parcel_large.png"
-                            alt="Large Parcel"
-                        />
-                        LARGE PARCEL
-                    </div>
-                    <div className={`${styles.parcel} ${styles.attention}`}>
-                        {/* eslint-disable-next-line */}
-                        <img
-                            src="https://dev.indejuice.com/img/wh/warning.png"
-                            alt="Attention"
-                        />
-                        <div>REQUIRES ATTENTION</div>
-                    </div>
-                </div>
+                <ParcelOptions />
             </div>
 
             {/* Footer */}
--- components/warehouse/packing/ParcelOptions.jsx
+++ components/warehouse/packing/ParcelOptions.jsx
@@ -1,20 +1,32 @@
 import styles from '@/styles/warehouse/packing/ParcelOptions.module.scss';
 
-// export default function ParcelOptions() {
-//   return (
-//     <div className={styles.parcelOptions}>
-//       <div className={`${styles.parcel} ${styles.small}`}>SMALL PARCEL</div>
-//       <div className={`${styles.parcel} ${styles.large}`}>LARGE PARCEL</div>
-//       <div className={styles.attention}>REQUIRES ATTENTION</div>
-//     </div>
-//   );
-// }
 export default function ParcelOptions() {
-    return (
-      <div className={styles.parcelOptions}>
-        <div className={`${styles.parcel} ${styles.small}`}>SMALL PARCEL</div>
-        <div className={`${styles.parcel} ${styles.large}`}>LARGE PARCEL</div>
-        <div className={styles.attention}>REQUIRES ATTENTION</div>
+  return (
+    <div className={styles.parcelOptions}>
+      <div className={`${styles.parcel} ${styles.small}`}>
+        {/* eslint-disable-next-line */}
+        <img
+          src="https://dev.indejuice.com/img/wh/parcel_small.png?v=2"
+          alt="Small Parcel"
+        />
+        SMALL PARCEL
       </div>
-    );
-  }
+      <div className={`${styles.parcel} ${styles.large}`}>
+        {/* eslint-disable-next-line */}
+        <img
+          src="https://dev.indejuice.com/img/wh/parcel_large.png"
+          alt="Large Parcel"
+        />
+        LARGE PARCEL
+      </div>
+      <div className={`${styles.parcel} ${styles.attention}`}>
+        {/* eslint-disable-next-line */}
+        <img
+          src="https://dev.indejuice.com/img/wh/warning.png"
+          alt="Attention"
+        />
+        <div>REQUIRES ATTENTION</div>
+      </div>
+    </div>
+  );
+}
