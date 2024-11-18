59b605061973cbfe1a85020f29e6ddbedb73f9ea
Yukenthan-indejuice <133008282+Yukenthan-indejuice@users.noreply.github.com>
2024-11-18 16:25:46 +0000

    Feature: Enhance PackingItems and PackingApp components with structured product listing and parcel options

--- components/warehouse/packing/PackingApp.jsx
+++ components/warehouse/packing/PackingApp.jsx
@@ -8,52 +8,35 @@ export default function PackingApp() {
             {/* Header */}
             <PackingHeader />
 
-            {/* Main Content */}
-            <div className={styles.content}>
+            {/* Main Section */}
+            <div className={styles.main}>
                 {/* Product List */}
-                <div className={styles.products}>
-                    {/* Product Item */}
-                    <div className={styles.productItem}>
-                        <div className={styles.imageContainer}>
-                            {/*  eslint-disable-next-line */}
-                            <img src="https://cdn.indejuice.com/images/GvS.jpg" alt="Cool Crush" />
-                            <div className={styles.quantity}>x1</div>
-                        </div>
-                        <div className={styles.productDetails}>
-                            <h3>Cool Crush</h3>
-                            <p>10ml | 3mg | 70VG/30PG</p>
-                        </div>
-                    </div>
-
-                    <div className={styles.productItem}>
-                        <div className={styles.imageContainer}>
-                            {/*  eslint-disable-next-line */}
-                            <img src="https://cdn.indejuice.com/images/GvS.jpg" alt="Avalanche" />
-                            <div className={styles.quantity}>x1</div>
-                        </div>
-                        <div className={styles.productDetails}>
-                            <h3>Avalanche</h3>
-                            <p>10ml | 3mg | 70VG/30PG</p>
-                        </div>
-                    </div>
-
-                    <div className={styles.productItem}>
-                        <div className={styles.imageContainer}>
-                            {/*  eslint-disable-next-line */}
-                            <img src="https://cdn.indejuice.com/images/GvS.jpg" alt="Berry Blast" />
-                            <div className={styles.quantity}>x1</div>
-                        </div>
-                        <div className={styles.productDetails}>
-                            <h3>Berry Blast</h3>
-                            <p>10ml | 3mg | 70VG/30PG</p>
-                        </div>
+                <div className={styles.content}>
+                    <div className={styles.products}>
+                        {/* Product Item */}
+                        {["Cool Crush", "Avalanche", "Berry Blast"].map((product, index) => (
+                            <div key={index} className={styles.productItem}>
+                                <div className={styles.imageContainer}>
+                                    {/* eslint-disable-next-line */}
+                                    <img
+                                        src="https://cdn.indejuice.com/images/GvS.jpg"
+                                        alt={product}
+                                    />
+                                    <div className={styles.quantity}>x1</div>
+                                </div>
+                                <div className={styles.productDetails}>
+                                    <h3>{product}</h3>
+                                    <p>10ml | 3mg | 70VG/30PG</p>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
 
                 {/* Parcel Options */}
                 <div className={styles.parcelOptions}>
                     <div className={`${styles.parcel} ${styles.small}`}>
-                        {/*  eslint-disable-next-line */}
+                        {/* eslint-disable-next-line */}
                         <img
                             src="https://dev.indejuice.com/img/wh/parcel_small.png?v=2"
                             alt="Small Parcel"
@@ -61,7 +44,7 @@ export default function PackingApp() {
                         SMALL PARCEL
                     </div>
                     <div className={`${styles.parcel} ${styles.large}`}>
-                        {/*  eslint-disable-next-line */}
+                        {/* eslint-disable-next-line */}
                         <img
                             src="https://dev.indejuice.com/img/wh/parcel_large.png"
                             alt="Large Parcel"
@@ -69,11 +52,12 @@ export default function PackingApp() {
                         LARGE PARCEL
                     </div>
                     <div className={`${styles.parcel} ${styles.attention}`}>
-                        {/*  eslint-disable-next-line */}
-                        <img src="https://dev.indejuice.com/img/wh/warning.png" alt="error" />
-                        <div className={``}>
-                            REQUIRES ATTENTION
-                        </div>
+                        {/* eslint-disable-next-line */}
+                        <img
+                            src="https://dev.indejuice.com/img/wh/warning.png"
+                            alt="Attention"
+                        />
+                        <div>REQUIRES ATTENTION</div>
                     </div>
                 </div>
             </div>
--- components/warehouse/packing/PackingItems.jsx
+++ components/warehouse/packing/PackingItems.jsx
@@ -1,26 +1,75 @@
 import styles from '@/styles/warehouse/packing/PackingItems.module.scss';
 
-// export default function PackingItems({ image, name, details, quantity }) {
-//   return (
-//     <div className={styles.productItem}>
-//       <img src={image} alt={name} className={styles.productImage} />
-//       <div className={styles.productDetails}>
-//         <h3>{name}</h3>
-//         <p>{details}</p>
-//       </div>
-//       <div className={styles.quantity}>x{quantity}</div>
-//     </div>
-//   );
-// }
+
 export default function PackingItems({ image, name, details, quantity }) {
     return (
-        <div className={styles.productItem}>
-            <img src={image} alt={name} className={styles.productImage} />
-            <div className={styles.productDetails}>
-                <h3>{name}</h3>
-                <p>{details}</p>
+        <div className={styles.content}>
+            {/* Product List */}
+            <div className={styles.products}>
+                {/* Product Item */}
+                <div className={styles.productItem}>
+                    <div className={styles.imageContainer}>
+                        {/*  eslint-disable-next-line */}
+                        <img src="https://cdn.indejuice.com/images/GvS.jpg" alt="Cool Crush" />
+                        <div className={styles.quantity}>x1</div>
+                    </div>
+                    <div className={styles.productDetails}>
+                        <h3>Cool Crush</h3>
+                        <p>10ml | 3mg | 70VG/30PG</p>
+                    </div>
+                </div>
+
+                <div className={styles.productItem}>
+                    <div className={styles.imageContainer}>
+                        {/*  eslint-disable-next-line */}
+                        <img src="https://cdn.indejuice.com/images/GvS.jpg" alt="Avalanche" />
+                        <div className={styles.quantity}>x1</div>
+                    </div>
+                    <div className={styles.productDetails}>
+                        <h3>Avalanche</h3>
+                        <p>10ml | 3mg | 70VG/30PG</p>
+                    </div>
+                </div>
+
+                <div className={styles.productItem}>
+                    <div className={styles.imageContainer}>
+                        {/*  eslint-disable-next-line */}
+                        <img src="https://cdn.indejuice.com/images/GvS.jpg" alt="Berry Blast" />
+                        <div className={styles.quantity}>x1</div>
+                    </div>
+                    <div className={styles.productDetails}>
+                        <h3>Berry Blast</h3>
+                        <p>10ml | 3mg | 70VG/30PG</p>
+                    </div>
+                </div>
             </div>
-            <div className={styles.quantity}>x{quantity}</div>
+
+            {/* Parcel Options */}
+            <div className={styles.parcelOptions}>
+                <div className={`${styles.parcel} ${styles.small}`}>
+                    {/*  eslint-disable-next-line */}
+                    <img
+                        src="https://dev.indejuice.com/img/wh/parcel_small.png?v=2"
+                        alt="Small Parcel"
+                    />
+                    SMALL PARCEL
+                </div>
+                <div className={`${styles.parcel} ${styles.large}`}>
+                    {/*  eslint-disable-next-line */}
+                    <img
+                        src="https://dev.indejuice.com/img/wh/parcel_large.png"
+                        alt="Large Parcel"
+                    />
+                    LARGE PARCEL
+                </div>
+                <div className={`${styles.parcel} ${styles.attention}`}>
+                    {/*  eslint-disable-next-line */}
+                    <img src="https://dev.indejuice.com/img/wh/warning.png" alt="error" />
+                    <div className={``}>
+                        REQUIRES ATTENTION
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
