219b1b8bb2c88f666b852a2559bfcc685d2d3a25
amannan-indejuice <abdul@indejuice.com>
2024-11-28 14:39:42 +0000

    Add function to retrieve all ISO Alpha-2 country codes with official names

--- services/utils/countries.js
+++ services/utils/countries.js
@@ -20,3 +20,15 @@ export const getOfficialCountryName = (countryCode) => {
   if (!countryCode || typeof countryCode !== 'string') return null;
   return countries.getName(countryCode.toUpperCase(), 'en', { select: 'official' }) || null;
 };
+
+/**
+ * Provides a list of all ISO Alpha-2 country codes with their official country names.
+ * @returns {Array<{ code: string, name: string }>} Array of country code and name objects.
+ */
+export const getAllCountriesWithNames = () => {
+  const countryNames = countries.getNames('en', { select: 'official' });
+  return Object.entries(countryNames).map(([code, name]) => ({
+    code: code.toUpperCase(),
+    name,
+  }));
+};
