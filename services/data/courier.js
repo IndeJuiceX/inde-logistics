fbd5c34012c552f758e1549bd19f279ef019a608
amannan-indejuice <abdul@indejuice.com>
2024-11-28 14:28:32 +0000

    Refactor shipping code validation and add vendor shipping codes retrieval

--- services/data/courier.js
+++ services/data/courier.js
@@ -20,15 +20,15 @@ export const validateOrderShippingCode = async (vendorId, order) => {
         const courierCodes = courierData?.data || null
         if (!courierCodes || courierCodes.length === 0) {
             //return { success: false, error: 'Invalid Shipping Code', details: "Please check shipping_code and try again" }
-            return {success: false, error : 'Invalid shipping_code, Please check shipping_code and try again' }
-         } //else {
+            return { success: false, error: 'Invalid shipping_code, Please check shipping_code and try again' }
+        } //else {
         //     return { success: true, data: { 'shipping_code': code, 'courier': courierCodes[0].courier_name, 'shipping_id': courierCodes[0].inde_shipping_id } };
         // }
-        if(order.buyer.country_code !== 'GB' && !order.shipping_code.includes('-INT')) {
-            return {success: false, error : 'Invalid shipping_code for international shipment, Please check shipping_code and try again' }
+        if (order.buyer.country_code !== 'GB' && !order.shipping_code.includes('-INT')) {
+            return { success: false, error: 'Invalid shipping_code for international shipment, Please check shipping_code and try again' }
 
         }
-        return {success:true}
+        return { success: true }
 
     } catch (error) {
         console.error('Error in createOrder:', error);
@@ -40,7 +40,7 @@ export const validateOrderShippingCode = async (vendorId, order) => {
     }
 };
 
-export const getCourierDetails = async (vendorId,code='RM-24') => {
+export const getCourierDetails = async (vendorId, code = 'RM-24') => {
     const vendorData = await getItem(`VENDOR#${vendorId}`, `VENDOR#${vendorId}`)
     const vendor = vendorData?.data || null
     if (!vendor) {
@@ -56,6 +56,29 @@ export const getCourierDetails = async (vendorId,code='RM-24') => {
     if (!courierCodes || courierCodes.length === 0) {
         return { success: false, error: 'Invalid Courier Code', details: "Courier code not known" }
     }
-    return {success: true, data:cleanResponseData(courierCodes)}
+    return { success: true, data: cleanResponseData(courierCodes) }
+}
+
+export const getVendorShippingCodes = async (vendorId) => {
+    const vendorData = await getItem(`VENDOR#${vendorId}`, `VENDOR#${vendorId}`)
+    const vendor = vendorData?.data || null
+    if (!vendor) {
+        return { success: false, error: 'Vendor not found', details: "Invalid vendor id" }
+    }
+    const vendorCourier = vendor?.courier || null
+    if (!vendorCourier) {
+        return { success: false, error: 'Vendor Courier is not set', details: "Please set the vendor courier and try again" }
+    }
+    const courierNameKey = vendorCourier.toLowerCase().replace(/\s+/g, '');
+    const courierData = await queryItemsWithPkAndSk(`COURIER#${courierNameKey}`, `CODE#`)
+    const courierCodesData = courierData?.data || null
+
+    if (!courierCodesData || courierCodesData.length === 0) {
+        //return { success: false, error: 'Invalid Shipping Code', details: "Please check shipping_code and try again" }
+        return { success: false, error: 'Vendor Courier has no data set' }
+    }
+    // Extract unique shipping codes
+    const uniqueShippingCodes = [...new Set(courierCodesData.map(codeData => codeData.shipping_code))];
+    return { success: true, data: { courier_name: vendorCourier, shipping_codes: uniqueShippingCodes } }
 }
 
